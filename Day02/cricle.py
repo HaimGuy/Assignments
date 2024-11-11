5653985e03062c5480dde7b8a22aa97d902f213a
HaimGuy <guy1986@gmail.com>
2024-11-11 10:15:01 +0200

    Add files via upload

---
+++ Day02/cricle.py
@@ -0,0 +1,6 @@
+import math
+radius = int(input ("what is the radius?"))
+area = math.pi * radius ** 2
+circ = 2 * math.pi * radius
+print ('Area of circle=', area)                
+print ('Circumference of circle=', circ)
---
+++ Day02/rectangle.py
@@ -0,0 +1,6 @@
+width = int(input ("what is the width?"))
+Length = int(input ("what is the Length?"))
+area = width * Length
+circ = (2 * width) + (2 * Length)
+print ('Area =', area)                
+print ('Circumference =', circ)
